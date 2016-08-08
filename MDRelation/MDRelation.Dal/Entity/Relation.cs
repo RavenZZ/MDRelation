ca23ebd87d2cdbc4a8b593b47c522ef36a0abf4d
Raven <ravenzyj@outlook.com>
2016-08-08 10:51:20 +0800

    init model

---
+++ MDRelation/MDRelation.Dal/Entity/Relation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MDRelation.Dal.Entity
+{
+    [BsonIgnoreExtraElements]
+    public class Relation
+    {
+        /// <summary>
+        /// 关联AccountID
+        /// </summary>
+        [BsonElement("aid")]
+        public string AccountId { get; set; }
+
+
+        /// <summary>
+        /// 关联类型
+        /// </summary>
+        [BsonElement("t")]
+        public int Type { get; set; }
+
+
+        /// <summary>
+        /// 关联实体ID
+        /// 如 任务ID, 群组ID 
+        /// </summary>
+        public string EntityId { get; set; }
+
+    }
+}
--- MDRelation/MDRelation.Dal/RelationProvider.cs
+++ MDRelation/MDRelation.Dal/RelationProvider.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace MDRelation.Dal
 {
-    public class Class1
+    public class RelationProvider
     {
-        public Class1()
+        public RelationProvider()
         {
         }
     }
